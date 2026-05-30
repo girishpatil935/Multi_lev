aef34692277a8b9514b82721dca4d0c8244cdb35
girishpatil935 <girishoffcial@gmail.com>
2026-05-30 20:23:17 +0530

    abstract usermodel added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,9 +37,34 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party apps
+     'rest_framework',
+     'corsheaders',
+     #local apps
+     'users',
+     'posts',
+     'comments',
+     'likes',
+     'follows',
+     'notifications',
+     'core',
+
+]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,3 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'users.User'
+Media_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
